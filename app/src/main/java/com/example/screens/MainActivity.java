23244eee40dc46750b5923ca1a1da58bbbaef48f
Jonathan Antfolk <61019188+walsand987@users.noreply.github.com>
2023-04-27 15:47:49 +0200

    added button for starting second activity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    // function to start second activity
+    public void startSecondActivity(android.view.View view) {
+        // create an intent to start second activity
+        android.content.Intent intent = new android.content.Intent(this, SecondActivity.class);
+        // add data to intent
+        intent.putExtra("com.example.screens.MESSAGE", "Hello from MainActivity!");
+
+        // start second activity
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        // set onClickListener for button
+        button.setOnClickListener(new android.view.View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View view) {
+                startSecondActivity(view);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,4 +3,21 @@ package com.example.screens;
 import android.app.Activity;
 
 public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        // get intent
+        android.content.Intent intent = getIntent();
+        // get data from intent
+        String message = intent.getStringExtra("com.example.screens.MESSAGE");
+
+        // set text of textView
+        android.widget.TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+
+        
+    }
 }
